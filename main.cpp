08639e08d2c4852d819fc469279a8ec04a90d3ea
jian.li <jian.li@shopeemobile.com>
2019-07-10 19:57:23 +0800

    add cmakelists and hello.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//
+// Created by 李剑 on 2019-07-10.
+//
+
+#include "main.h"
+#include <stdio.h>
+
+int main(void)
+
+{
+    printf("Hello,World!");
+    return 0;
+
+}
---
+++ main.h
@@ -0,0 +1,13 @@
+//
+// Created by 李剑 on 2019-07-10.
+//
+
+#ifndef HELLO_MAIN_H
+#define HELLO_MAIN_H
+
+
+class main {
+};
+
+
+#endif //HELLO_MAIN_H
