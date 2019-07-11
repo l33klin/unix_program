913d81c2da988ad42a3b4a74e50bacffe7334d28
l33klin <l33klin@gmail.com>
2019-07-11 23:53:00 +0800

    add include and apue.cpp

--- main.cpp
+++ main.cpp
@@ -3,12 +3,23 @@
 //
 
 #include "main.h"
+#include "include/apue.h"
+#include <dirent.h>
 #include <stdio.h>
 
-int main(void)
-
+int main(int argc, char *argv[])
 {
-    printf("Hello,World!");
-    return 0;
+    DIR				*dp;
+    struct dirent	*dirp;
 
+    if (argc != 2)
+        err_quit("usage: ls directory_name");
+
+    if ((dp = opendir(argv[1])) == NULL)
+        err_sys("can't open %s", argv[1]);
+    while ((dirp = readdir(dp)) != NULL)
+        printf("%s\n", dirp->d_name);
+
+    closedir(dp);
+    exit(0);
 }
